ecf6a29a0cf250616f2b5160230d2de0542712bc
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-07-31 22:45:15 +0700

    Connecting with sql server; change some attributes in application.properties, data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,13 @@
-insert into employee(id, name, contact_number, office)
-values(1001L, 'Employee1', '1111', 'Office1');
-insert into employee(id, name, contact_number, office)
-values(1002L, 'Employee2', '2222', 'Office2');
-insert into employee(id, name, contact_number, office)
-values(1003L, 'Employee3', '3333', 'Office3');
+-- SET IDENTITY_INSERT employee ON;
+-- insert into employee(id, name, contact_number, office)
+-- values(1001, 'Employee1', '1111', 'Office1');
+-- insert into employee(id, name, contact_number, office)
+-- values(1002, 'Employee2', '2222', 'Office2');
+-- insert into employee(id, name, contact_number, office)
+-- values(1003, 'Employee3', '3333', 'Office3');
+-- SET IDENTITY_INSERT employee OFF;
 
+insert into employee(name, contact_number, office) values('Employee1', '1111', 'Office1');
+insert into employee(name, contact_number, office) values('Employee2', '2222', 'Office2');
+insert into employee(name, contact_number, office) values('Employee3', '3333', 'Office3');
 
